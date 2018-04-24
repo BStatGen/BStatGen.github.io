9b9c4e47112c6e7dc0adec25fc0d317ab9655357
BStatGen <38413987+BStatGen@users.noreply.github.com>
2018-04-24 18:15:30 +0800

    deletyed

--- bsg.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById("Score1");
-var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("SCORE:",10,50);
-
-var canvas = document.getElementById("Score2");
-var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("SCORE:",10,50);
